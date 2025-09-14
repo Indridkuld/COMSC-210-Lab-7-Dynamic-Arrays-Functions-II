1312a69c9c4d0fa57cd1b22a373e25f302c236ae
Aidan <aidanmichaelwoodcock@gmail.com>
2025-09-14 14:46:52 -0700

    update

--- main.cpp
+++ main.cpp
@@ -12,7 +12,24 @@ void displayArray(string *arr, int size);
 int main() {
     const int size = 5;
     string *dsArr = nullptr;
-    dsArr = new string[size]{"Janet", "Jeffe", "Jin", "Joe", "Junio"};    
+    dsArr = new string[size];
+    char choice;
+
+    cout << "Would you like to enter string instead of using default? (Y/N): ";
+
+    if(choice == 'Y' || choice == 'y') {
+        cout << "Enter 5 names: ";
+        for (int i = 0; i < size; i++) {
+            cin >> *(dsArr + i);
+        }
+    } else {
+        cout << "Using default names.\n";
+        *(dsArr + 0) = "Janet";
+        *(dsArr + 1) = "Jeffe";
+        *(dsArr + 2) = "Jin";
+        *(dsArr + 3) = "Joe";
+        *(dsArr + 4) = "Junio";
+    }
 
     cout << "Original array: ";
     displayArray(dsArr, size);
@@ -27,16 +44,18 @@ int main() {
 }
 // function definitions
 string *reverseArray(string *arr, int size) {
-    string *start = arr;
-    string *end = arr + size -1;
+    string* start = arr;
+    string* end = arr + size - 1;
     while (start < end) {
         swap(*start, *end);
         start++;
         end--;
     }
+    return arr;
 }
 void displayArray(string *arr, int size) {
     for (int i = 0; i < size; i++) {
-        cout << *(arr + 1) << " ";
+        cout << *(arr + i) << " "; 
+    cout << endl;
     }
 }
