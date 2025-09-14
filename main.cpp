1d64a3809fa105a02c99cbbc77d2703a430e1dd0
Aidan <aidanmichaelwoodcock@gmail.com>
2025-09-14 13:09:25 -0700

    update

---
+++ main.cpp
@@ -0,0 +1,47 @@
+// COMSC-210 | Lab 7: Dynamic Arrays & Functions II | Aidan Woodcock | 2025-09-14
+// IDE used: Visual Studio Code
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+// Function prototypes
+void reverseArray(string* arr, int size);
+void displayArray(string* arr, int size);
+
+int main() {
+    const int size = 5;
+    string* dsArr = new string[size];
+
+    cout << "Enter " << size << " strings:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "String " << (i + 1) << ": ";
+        getline(cin, dsArr[i]);
+    }
+    reverseArray(dsArr, size);
+    displayArray(dsArr, size);
+
+    delete[] dsArr;
+
+    return 0;
+}
+// function definitions
+void reverseArray(string* arr, int size) {
+
+
+}
+void displayArray(string* arr, int size) {
+    string* tempArr = new string[size];
+    for (int i = 0; i < size; i++) {
+        tempArr[i] = arr[size - 1 - i];
+    }
+    cout << "\nOriginal array: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << endl;
+    }
+    cout << "\nReversed array: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << tempArr[i] << endl;
+    }
+}
