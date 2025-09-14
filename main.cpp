df99de40c56bb021ff98350d4a4fa272043237f7
Aidan <aidanmichaelwoodcock@gmail.com>
2025-09-14 15:01:38 -0700

    update: final update and end of coding session

--- main.cpp
+++ main.cpp
@@ -8,15 +8,16 @@ using namespace std;
 // Function prototypes
 string *reverseArray(string *arr, int size);
 void displayArray(string *arr, int size);
-
+// creates dynamic string array, user submits strings or uses default, displays original and reversed array
 int main() {
     const int size = 5;
     string *dsArr = nullptr;
     dsArr = new string[size];
     char choice;
-
+    // gave user option to submit own strings or use default sample strings 
     cout << "Would you like to enter string instead of using default? (Y/N): ";
-
+    cin >> choice;
+    cin.ignore();
     if(choice == 'Y' || choice == 'y') {
         cout << "Enter 5 names: ";
         for (int i = 0; i < size; i++) {
@@ -36,13 +37,14 @@ int main() {
 
     dsArr = reverseArray(dsArr, size);
 
-    cout << "\nReversed array: ";
+    cout << "Reversed array: ";
     displayArray(dsArr, size);
 
     delete[] dsArr;
     return 0;
 }
 // function definitions
+// reverses the order of elements in a dynamic string array using pointers
 string *reverseArray(string *arr, int size) {
     string* start = arr;
     string* end = arr + size - 1;
@@ -53,9 +55,10 @@ string *reverseArray(string *arr, int size) {
     }
     return arr;
 }
+// displays elements of a dynamic string array using pointers
 void displayArray(string *arr, int size) {
     for (int i = 0; i < size; i++) {
         cout << *(arr + i) << " "; 
-    cout << endl;
     }
+    cout << endl;
 }
